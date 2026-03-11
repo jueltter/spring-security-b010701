e9ac6dcaf40570b41492b2d14438edf229c79087
Stalin Amagua <jueltter@users.noreply.github.com>
2026-03-10 23:57:12 -0500

    - config added

---
+++ src/main/java/tech/samagua/spring_security_b010701/controllers/HelloController.java
@@ -0,0 +1,14 @@
+package tech.samagua.spring_security_b010701.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
+
+}
